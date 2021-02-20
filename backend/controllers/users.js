88fb070a854d92c77ede9eb953092a69a7580bbd
Lia <liacicati@gmail.com>
2021-02-20 13:26:36 +0100

    feat : users controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,69 +1,71 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+// const InternalServerError = require('../errors/InternalServerError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
+const ConflictError = require('../errors/ConflictError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => {
-      res.status(200).send({ users });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: `Запрашиваемый ресурс не найден ${err}` });
-    });
-};
-
-const getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .orFail(new Error('Не найдено'))
-    .then((user) => res.send({ user }))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Введён неправильный id' });
+      if (!users) {
+        throw new NotFoundError('Запрашиваемый ресурс не найден');
       } else {
-        res.status(500).send({ message: 'Ошибка сервера' });
+        res.send({ users });
       }
-    });
+    })
+    .catch(next);
 };
 
-const getUserMe = (req, res, next) => {
-  User.findById(req.user._id)
+const getUserById = (req, res, next) => {
+  User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        console.log('Нет пользователя с таким id');
+        throw new NotFoundError('Нет пользователя с таким id');
       } else {
-        res.send(user);
+        res.send({ user });
       }
     })
     .catch((err) => {
-      if (err.kind === 'ObjectId') {
-        next(console.log('Неверно введен id'));
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Неверно введен id'));
       }
       next(err);
     });
 };
 
-const createUser = (req, res) => {
+const getUserMe = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(() => { throw new NotFoundError('Пользователь не найден'); })
+    .then((user) => res.send({ data: user }))
+    .catch(next);
+};
+
+const createUser = (req, res, next) => {
   const {
-    name, about, avatar, password, email,
+    name, about, avatar, email, password,
   } = req.body;
-  if (req.body.password.length < 8) {
-    console.log('Ошибка валидации. Пароль должен состоять из 8 или более символов');
-  } else {
-    bcrypt
-      .hash(password.toString(), 10)
-      .then((hash) => User.create({
-        name, about, avatar, email, password: hash,
+  if (!email || !password) { throw new BadRequestError('Не предоставлены email или пароль'); }
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        throw new ConflictError('Пользователь с таким email адресом уже зарегистрирован');
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    })
+      .then(({ _id }) => {
+        res.status(200).send({
+          _id, email, name, avatar, about,
+        });
       }))
-      .then((user) => res.status(200).send(user))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          return res.status(400).send({ message: 'Ошибка валидации' });
-        }
-        return res.status(500).send({ message: 'Ошибка сервера' });
-      });
-  }
+    .catch(next);
 };
 
 const login = (req, res, next) => {
@@ -76,7 +78,7 @@ const login = (req, res, next) => {
       );
       res.send({ token });
     })
-    .catch(() => next(console.log('Введены неверное имя или пароль')));
+    .catch(() => next(new BadRequestError('Введены неверное имя или пароль')));
 };
 
 module.exports = {
