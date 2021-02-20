3d37a9ddd51a3a4df3f968d4c65ef93d7272ba2c
Lia <liacicati@gmail.com>
2021-02-20 13:29:28 +0100

    fixed : errorHandler

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+/* eslint-disable consistent-return */
+// eslint-disable-next-line consistent-return
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  if (res.status) {
+    return res.status(err.status).send({ message: err.message });
+  }
+  res.status(500).send({ message: err.message });
+};
+
+module.exports = errorHandler;
