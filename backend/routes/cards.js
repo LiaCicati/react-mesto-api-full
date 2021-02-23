76ef7339ba5159923ebce4ce53aff622997c510a
Lia <liacicati@gmail.com>
2021-02-23 13:41:01 +0100

    like/dislike card

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { validateCard, validateСardId } = require('../middlewares/celebrateValidation');
-const { getCards, createCard, deleteCard } = require('../controllers/cards.js');
+const {
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
+} = require('../controllers/cards.js');
 
 router.get('/', getCards);
 router.post('/', validateCard, createCard);
 router.delete('/:cardId', validateСardId, deleteCard);
+router.put('/:cardId/likes', validateСardId, likeCard);
+router.delete('/:cardId/likes', validateСardId, dislikeCard);
 
 module.exports = router;
