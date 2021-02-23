59c63be7556b5c28ce87cfc5a03013c0a631c749
Lia <liacicati@gmail.com>
2021-02-23 16:16:16 +0100

    fixed : changed some styles

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -39,7 +39,7 @@ function App() {
 
   useEffect(() => {
     tokenCheck();
-  }, []);
+  });
 
   function tokenCheck() {
     const token = localStorage.getItem("token");
@@ -104,6 +104,32 @@ function App() {
         }
       });
   }
+
+  // const onLogin = (emailUser, password) => {
+  //   // авторизация
+  //   auth.authorize(emailUser, password)
+  //     .then((res) => {
+  //       // eslint-disable-next-line consistent-return
+  //       auth.getContent(res.data.token).then((user) => {
+  //         if (user.data) {
+  //           setCurrentUser(user.data);
+
+  //           localStorage.setItem('token', res.data.token);
+  //           setEmail(emailUser);
+  //           setLoggedIn(true);
+  //         } else {
+  //           return new Promise().reject();
+  //         }
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       if (err.data) {
+  //         onOpenPopupInfoTooltip(false, err.data.message);
+  //       } else {
+  //         onOpenPopupInfoTooltip(false, 'Что-то пошло не так');
+  //       }
+  //     });
+  // };
 
   function onSignOut() {
     localStorage.removeItem("token");
